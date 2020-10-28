82eb87410284205641b90d14b005fd54b20b5605
Marcio Valle De Oliveira e Cruz <mvocruz@hotmail.com>
2020-10-28 03:15:59 +0000

    lotida project  - Equalizing arrays

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  let message = '';
+  if (actual !== expected) {
+    message += console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    message += console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  return message;
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
